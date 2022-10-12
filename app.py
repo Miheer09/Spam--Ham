6f79aaec10e39438759600118bb2a2c2cbb30f69
Miheer09 <88573473+Miheer09@users.noreply.github.com>
2022-10-12 20:48:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ ip = st.text_input('Enter The message')#It Creates a text box
 op = model.predict([ip])
 if st.button('Predict'):
  st.title(op[0])# the 0th element in op variable is display as a title
+
+ 
+ 
+ 
+ 
